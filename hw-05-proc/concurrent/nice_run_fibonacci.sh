2d36d137e23bde668494c96be478875811d8cc8a
Aleksandr Demshin <demshin@gmail.com>
2020-03-17 11:26:46 +0500

    test nice

---
+++ hw-05-proc/concurrent/nice_run_fibonacci.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "started '$(date)'" > nice.log
+
+high_priority () {
+    nice -n-20 ./fibonacci.sh 20
+    echo "process with high priority finished at '$(date)'" >> nice.log
+}
+
+low_priority () {
+    nice -n19 ./fibonacci.sh 20
+    echo "process with low priority finished at '$(date)'" >> nice.log
+}
+
+high_priority &
+low_priority &
+wait
